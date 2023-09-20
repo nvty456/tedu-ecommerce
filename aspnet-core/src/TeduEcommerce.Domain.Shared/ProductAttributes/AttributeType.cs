dcdae771bbd8ecee9a06550c593608533806aa03
nvty456 <nguyenvanty.dev@gmail.com>
2023-09-20 21:22:07 +0700

    update lesson/4.7

---
+++ aspnet-core/src/TeduEcommerce.Domain.Shared/ProductAttributes/AttributeType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TeduEcommerce.ProductAttributes
+{
+    public enum AttributeType
+    {
+        Date,
+        Varchar,
+        Text,
+        Int,
+        Decimal
+    }
+}
--- aspnet-core/src/TeduEcommerce.EntityFrameworkCore/Migrations/20230911155627_CreateBussinessEntities.Designer.cs
+++ aspnet-core/src/TeduEcommerce.EntityFrameworkCore/Migrations/20230911155627_CreateBussinessEntities.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using TeduEcommerce.EntityFrameworkCore;
 using Volo.Abp.EntityFrameworkCore;
@@ -12,9 +13,11 @@ using Volo.Abp.EntityFrameworkCore;
 namespace TeduEcommerce.Migrations
 {
     [DbContext(typeof(TeduEcommerceDbContext))]
-    partial class TeduEcommerceDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230911155627_CreateBussinessEntities")]
+    partial class CreateBussinessEntities
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
